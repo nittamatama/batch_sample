a587db158425b9c6cb625d8a68531735b8eb8d08
nittamatama <blackmnshin@gmail.com>
2022-02-06 22:50:29 +0900

    マイグレーションファイルを３つ作成

---
+++ db/migrate/20220206134930_create_user_scores.rb
@@ -0,0 +1,6 @@
+class CreateUserScores < ActiveRecord::Migration[6.0]
+  def change
+    create_table :user_scores do |t|
+    end
+  end
+end
---
+++ db/migrate/20220206134939_create_ranks.rb
@@ -0,0 +1,6 @@
+class CreateRanks < ActiveRecord::Migration[6.0]
+  def change
+    create_table :ranks do |t|
+    end
+  end
+end
